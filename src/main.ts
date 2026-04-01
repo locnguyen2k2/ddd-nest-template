e50181703f4284bf4af0d0c0073b5278677459ad
Loc Nguyen <locnguyen071102@gmail.com>
2026-04-02 00:36:32 +0700

    update project

--- src/main.ts
+++ src/main.ts
@@ -61,10 +61,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://cjool-admin-dashboard.vercel.app',
-    ],
+    origin: ["*"],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
